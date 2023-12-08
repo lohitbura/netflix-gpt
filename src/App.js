22ba0816f3639fe3c573f83eee550429cf02015c
Lohit Bura <lohitbura@gmail.com>
2023-12-08 14:47:46 +0530

    Initial Project and Tailwind Setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1 className="text-3xl font-bold text-green-900">Netflix GPT</h1>
+    </div>
+  );
+}
+
+export default App;
